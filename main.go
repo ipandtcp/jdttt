7fbaf54147e2c8ec6d0633693f1762281f360896
Kevin Zhu <ipandtcp@gmail.com>
2025-07-15 15:14:46 +0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func main() {
+	resp, err := http.Get("https://aaa.com")
+	if err != nil {
+		fmt.Println("请求出错:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("状态码:", resp.Status)
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("读取响应失败:", err)
+		return
+	}
+
+	fmt.Println("响应内容:")
+	fmt.Println(string(body))
+}
